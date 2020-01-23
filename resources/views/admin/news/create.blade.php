2ff59459c9f7718d8c7fcbe2aa087af2b8e9ebe8
iwasaki <arlinesion@yahoo.co.jp>
2020-01-23 05:02:49 +0000

    test

---
+++ resources/views/admin/news/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta chaeset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <mata name="viewport" content="width=device-width, initial-scale=1">
+      <title></title>
+  </head>
+  
+  <body>
+    <h1>Myニュース作成画面</h1>
+  </body>
+</html>
